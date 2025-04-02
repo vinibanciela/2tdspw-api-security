613bb474eb1ef8edaec9e2abd5a8fe0137868465
vinibanciela <vinibanciela@gmail.com>
2025-04-01 22:46:24 -0300

    add a classe enum UserRole

---
+++ src/main/java/br/com/fiap/apisecurity/model/UserRole.java
@@ -0,0 +1,23 @@
+package br.com.fiap.apisecurity.model;
+
+public enum UserRole {
+    ADMIN("admin"),
+    USER("user");
+
+    private final String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    @Override
+    public String toString() {
+        return "UserRole{" +
+                "role='" + role + '\'' +
+                '}';
+    }
+}
