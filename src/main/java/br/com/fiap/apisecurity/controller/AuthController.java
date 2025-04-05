5ff2e87edcdaf2a9721cff125ed4bfde1b322fb9
vinibanciela <vinibanciela@gmail.com>
2025-04-04 22:59:00 -0300

    add a classe AuthController na pasta controller

---
+++ src/main/java/br/com/fiap/apisecurity/controller/AuthController.java
@@ -0,0 +1,37 @@
+package br.com.fiap.apisecurity.controller;
+
+import br.com.fiap.apisecurity.dto.AuthDTO;
+import br.com.fiap.apisecurity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid AuthDTO authDTO) {
+        var userPwd = new UsernamePasswordAuthenticationToken(
+                authDTO.username()
+                authDTO.password());
+        var auth = this.authenticationManager.authenticate(userPwd);
+        return ResponseEntity.ok().build()
+    }
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody @Valid)
+        if (userRepository.findByUsername(registerDTO.username()) != null) {
+            return responseEntity.badRequest().build();
+    }
+        String encryptedPwd = new BCryptPasswordencoder().encode(registerDTO.password());
+}
+
