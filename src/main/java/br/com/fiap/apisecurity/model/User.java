a8d18bdd39cd6b17e5824bfaf5f2de3af42ba6de
vinibanciela <vinibanciela@gmail.com>
2025-04-15 22:46:28 -0300

    configurações em User de Model para habilitar autenticação

--- src/main/java/br/com/fiap/apisecurity/model/User.java
+++ src/main/java/br/com/fiap/apisecurity/model/User.java
@@ -19,6 +19,8 @@ public class User implements UserDetails {
     private String password;
     private UserRole role;
 
+    public User() {}
+
     public User(String username, String password, UserRole role) {
         this.username = username;
         this.password = password;
@@ -37,31 +39,31 @@ public class User implements UserDetails {
 
     @Override
     public String getPassword() {
-        return "";
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return username;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return UserDetails.super.isAccountNonExpired();
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return UserDetails.super.isAccountNonLocked();
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return UserDetails.super.isCredentialsNonExpired();
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return UserDetails.super.isEnabled();
+        return true;
     }
 }
