79d702c6f2de2cca49c166cc789bfbdf35464df2
vinibanciela <vinibanciela@gmail.com>
2025-04-22 23:06:03 -0300

    editando classe AuthController para add como componente o SecutiryFilter, e add Token

--- src/main/java/br/com/fiap/apisecurity/controller/AuthController.java
+++ src/main/java/br/com/fiap/apisecurity/controller/AuthController.java
@@ -1,37 +1,47 @@
-package br.com.fiap.apisecurity.controller;
+package br.com.fiap.apisecurity.security;
 
-import br.com.fiap.apisecurity.dto.AuthDTO;
-import br.com.fiap.apisecurity.dto.RegisterDTO;
-import br.com.fiap.apisecurity.model.User;
 import br.com.fiap.apisecurity.repository.UserRepository;
-import jakarta.validation.Valid;
-import org.apache.coyote.Response;
+import br.com.fiap.apisecurity.service.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-@RestController
-@RequestMapping("/auth")
-public class AuthController {
-    @Autowired
-    private AuthenticationManager authenticationManager;
-    @Autowired
-    private UserRepository userRepository;
+import java.io.IOException;
 
-    @PostMapping("/login")
-    public ResponseEntity login(@RequestBody @Valid AuthDTO authDTO) {
-        var userPwd = new UsernamePasswordAuthenticationToken(
-                authDTO.username(),
-                authDTO.password());
-                encryptedPwd,
-                registerDTO.role());
-        userRepository.save(newUser);
-        return ResponseEntity.ok().build();
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    private final TokenService tokenService;
+    private final UserRepository userRepository;
+    @Autowired
+    public SecurityFilter(TokenService tokenService, UserRepository userRepository) {
+        this.tokenService = tokenService;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+        if (token != null) {
+            var username = tokenService.validateToken(token);
+            UserDetails userDetails = this.userRepository.findByUsername(username);
+            var authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var header = request.getHeader("Authorization"); // "Bearer jdhkjsdhfjghsdfghjsdhfgshdfgh"
+        if (header == null || !header.startsWith("Bearer ")) {
+            return null;
+        }
+        return header.replace("Bearer ", ""); //"jdhkjsdhfjghsdfghjsdhfgshdfgh"
     }
 }
