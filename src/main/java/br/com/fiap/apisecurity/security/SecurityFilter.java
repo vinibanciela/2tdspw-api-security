8bf9c3a5d4ccf463878b6100ddeaa1b11d68af12
vinibanciela <vinibanciela@gmail.com>
2025-04-22 23:00:16 -0300

    codificando a classe SecurityFilter - retificação

--- src/main/java/br/com/fiap/apisecurity/security/SecurityFilter.java
+++ src/main/java/br/com/fiap/apisecurity/security/SecurityFilter.java
@@ -1,4 +1,47 @@
 package br.com.fiap.apisecurity.security;
 
-public class SecurityFilter {
+import br.com.fiap.apisecurity.repository.UserRepository;
+import br.com.fiap.apisecurity.service.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    private final TokenService tokenService;
+    private final UserRepository userRepository;
+    @Autowired
+    public SecurityFilter(TokenService tokenService, UserRepository userRepository) {
+        this.tokenService = tokenService;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = this.recoverToken(request);
+        if (token != null) {
+            var username = tokenService.validateToken(token);
+            UserDetails userDetails = this.userRepository.findByUsername(username);
+            var authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var header = request.getHeader("Authorization"); // "Bearer jdhkjsdhfjghsdfghjsdhfgshdfgh"
+        if (header == null || !header.startsWith("Bearer ")) {
+            return null;
+        }
+        return header.replace("Bearer ", ""); //"jdhkjsdhfjghsdfghjsdhfgshdfgh"
+    }
 }
